ba0635657d9a4d8ef548fd68b10c4ae250f2ad54
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-08-11 21:10:39 -0300

    Add project files.

---
+++ AulaPA_ExercicioPratico1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AulaPA_ExercicioPratico1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o modelo de seu veículo?");
+            String modelo = Console.ReadLine();
+
+            Console.WriteLine("Qual a marca de seu veículo?");
+            String marca = Console.ReadLine();
+
+            Console.WriteLine("Qual a cor de seu veículo?");
+            String cor = Console.ReadLine();
+
+            Console.WriteLine("Qual o ano de seu veículo?");
+            String ano = Console.ReadLine();
+
+            Console.WriteLine("Qual a quilometragem de seu veículo?");
+            String km = Console.ReadLine();
+
+            Console.Write("\n\nO modelo de seu carro é: " + modelo
+                + "\n\nSua marca é: " + marca
+                + "\n\nA cor de seu carro é: " + cor
+                + "\n\nO ano de seu carro é: " + ano
+                + "\n\nA quilometragem de seu carro é: " + km);
+
+            Console.ReadKey();
+        }
+    }
+}
